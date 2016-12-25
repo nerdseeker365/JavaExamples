4ce0d7dbf854a5b26d596fcdb49f9c0bb18577a2
John Doe <johndoe@example.com>
2016-12-25 12:23:32 +0530

    file added

--- src/basic/HelloWorld.java
+++ src/basic/HelloWorld.java
@@ -1,8 +1,13 @@
 package basic;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello, world");
+	public static void main(String[] argv) {
+		System.out.println("Hello, World");
 	}
-
 }
+/*
+output
+==================
+Hello, World
+
+*/
